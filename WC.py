c28af18938964a09e21fa3185eab4fef51daa4bc
Konstantin <111602710+yokstik@users.noreply.github.com>
2023-09-10 01:04:51 +0500

    Create WC.py

---
+++ WC.py
@@ -0,0 +1,10 @@
+input_str = '0100010100001'
+print(input_str)
+wc = list(input_str)
+
+for i in range(len(wc)):
+    if i % 2:
+        if wc[i-1] == '0' and wc[i] == '0' and wc[i+1] == '0':
+            wc[i] = '1'
+one = ''.join(wc)
+print(one)
