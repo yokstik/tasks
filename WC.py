862eef24fbc0d8f694d6e959fc41f72c24edd0c1
Konstantin <111602710+yokstik@users.noreply.github.com>
2023-09-10 01:13:45 +0500

    Update WC.py

--- WC.py
+++ WC.py
@@ -3,8 +3,7 @@ print(input_str)
 wc = list(input_str)
 
 for i in range(len(wc)):
-    if i % 2:
-        if wc[i-1] == '0' and wc[i] == '0' and wc[i+1] == '0':
-            wc[i] = '1'
+    if wc[i-1] == '0' and wc[i] == '0' and wc[i+1] == '0':
+        wc[i] = '1'
 one = ''.join(wc)
 print(one)
