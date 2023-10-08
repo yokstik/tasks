9a818c549931e87498ba5708486be9aaf944c3ec
Konstantin <111602710+yokstik@users.noreply.github.com>
2023-10-08 15:26:53 +0500

    Update wc2.py

--- wc2.py
+++ wc2.py
@@ -1,22 +1,22 @@
-wc = list('00010')
-long = len(wc)
+input_str = '010001'
+wc = list(input_str)
 
 print(wc)
 
 if '1' in wc:
-	if not long%2:
+	for i in range(1, len(wc) - 1):
 		if wc[0] == '1':
-			print(f'Кол-во четное, 1')
+			if wc[i - 1] == '0' and wc[i] == '0':
+				wc[i] = '1'
+				print(f' Позиция: {i}')
 		elif wc[0] == '0':
-			print(f'Кол-во четное, 0')
-	else:
-		if wc[0] == '1':
-			print(f'Кол-во не четное, 1')
-		elif wc[0] == '0':
-			print(f'Кол-во не четное, 0')
+			if wc[i - 1] == '0' and wc[i] == '0' and wc[i + 1] == '0':
+				wc[i] = '1'
+				print(f' Позиция: {i}')
 else:
 	for i in range(long):
 		if not i%2:
 			wc[i] = '1'
 
 print(wc)
+
