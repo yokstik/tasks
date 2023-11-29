431cac67f4d5ed69c2289f8e1e166234c9a443de
Konstantin <111602710+yokstik@users.noreply.github.com>
2023-11-29 13:22:30 +0500

    Update wc2.py

--- wc2.py
+++ wc2.py
@@ -1,22 +1,22 @@
 input_str = '010001'
-wc = list(input_str)
+wc = [int(i) for i in input_str]
 
-print(wc)
+def main():
+    print(wc)
+    long = len(wc)
+    step = long - 1
+    plus = 0
+    while step > 0:
+        for i in range(1, step):
+            peace = [wc[i - 1], wc[i], wc[i + 1]]
+            if peace == [0,0,0]:
+                print(f'Позиция -> {i}')
+                wc[i] = 1
+                plus += 1
+        step -= 1
+    if plus == 0:
+        print('Нет места')
+    print(wc)
 
-if '1' in wc:
-	for i in range(1, len(wc) - 1):
-		if wc[0] == '1':
-			if wc[i - 1] == '0' and wc[i] == '0':
-				wc[i] = '1'
-				print(f' Позиция: {i}')
-		elif wc[0] == '0':
-			if wc[i - 1] == '0' and wc[i] == '0' and wc[i + 1] == '0':
-				wc[i] = '1'
-				print(f' Позиция: {i}')
-else:
-	for i in range(long):
-		if not i%2:
-			wc[i] = '1'
 
-print(wc)
-
+main()
